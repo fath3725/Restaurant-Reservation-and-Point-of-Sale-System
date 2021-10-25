12493a1b546ba6399d08bad4f239c064988d172d
MarcusOCG <93039726+MarcusOCG@users.noreply.github.com>
2021-10-25 21:38:21 +0800

    Update Member.java

--- Member.java
+++ Member.java
@@ -1,29 +1,72 @@
 public class Member implements Person {
 
-	private String phoneNumber;
-	private string cardID;
+	private int phoneNumber;
+	private int cardID;
+	
+	public Person(String name, String gender, String id, int phoneNumber, int cardID){
+		this.name = name;
+		this.gender = gender;
+		this.id = id;
+		this.phoneNumber = phoneNumber;
+		this.cardID = cardID;
+	}
+	public String getName() {
+		return this.name;
+	}
 
-	public String getPhoneNumber() {
+	/**
+	 * Set member's name
+	 * @param String name
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getGender() {
+		return this.gender;
+	}
+
+	/**
+	 * Set member's gender
+	 * @param String gender
+	 */
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	
+	public String getID() {
+		return this.id;
+	}
+
+	/**
+	 * Set member's ID
+	 * @param String ID
+	 */
+	public void setID(String id) {
+		this.id = id;
+	}
+	
+	public int getPhoneNumber() {
 		return this.phoneNumber;
 	}
 
 	/**
-	 * 
-	 * @param phoneNumber
+	 * Set member's phoneNumber
+	 * @param int phoneNumber
 	 */
-	public void setPhoneNumber(String phoneNumber) {
+	public void setPhoneNumber(int phoneNumber) {
 		this.phoneNumber = phoneNumber;
 	}
 
-	public string getCardID() {
+	public int getCardID() {
 		return this.cardID;
 	}
 
 	/**
-	 * 
-	 * @param cardID
+	 * Set member's cardID
+	 * @param int cardID
 	 */
-	public void setCardID(string cardID) {
+	public void setCardID(int cardID) {
 		this.cardID = cardID;
 	}
 
