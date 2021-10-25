7e0aadf39aa2f3f144c481e9bf45f80de5b17103
MarcusOCG <93039726+MarcusOCG@users.noreply.github.com>
2021-10-25 20:33:25 +0800

    Add files via upload

---
+++ AlaCarte.java
@@ -0,0 +1,24 @@
+public class AlaCarte extends MenuItem {
+
+	private Category category;
+
+	public AlaCarte() {
+		// TODO - implement AlaCarte.AlaCarte
+		throw new UnsupportedOperationException();
+	}
+
+	public String getCategory() {
+		// TODO - implement AlaCarte.getCategory
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param category
+	 */
+	public void setCategory(String category) {
+		// TODO - implement AlaCarte.setCategory
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ Category.java
@@ -0,0 +1,5 @@
+public enum Category {
+	MAIN COURSE,
+	DRINK,
+	DESSERT
+}
