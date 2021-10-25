f996aca82e82735a28cd4621e8d1d7c7ea2868ad
MarcusOCG <93039726+MarcusOCG@users.noreply.github.com>
2021-10-25 22:22:58 +0800

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Menu {
 
 	private ArrayList<AlaCarte> alaCartes;
@@ -5,17 +7,55 @@ public class Menu {
 	private ArrayList<PromotionPackage> promotionPackages;
 
 	public Menu() {
-		// TODO - implement Menu.Menu
+		alaCartes = new ArrayList<AlaCarte>();
+		packages = new ArrayList<Package>();
+		promotionPackages = new ArrayList<PromotionPackage>();
 		throw new UnsupportedOperationException();
 	}
 
 	public void addMenuItems() {
-		// TODO - implement Menu.addMenuItems
-		throw new UnsupportedOperationException();
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Choose which of the following to remove: ");
+		System.out.println("1) Ala Carte");
+		System.out.println("2) Package");
+		System.out.println("3) Promotion Package");
+		int choice = sc.nextInt();
+		
+		switch(choice) {
+			case 1:
+				System.out.println("Enter menu item's name: ");
+				String foodName = sc.nextString();
+				System.out.println("Enter menu item's category: MAIN COURSE/DRINK/DESSERT ");
+				Category foodCategory = sc.nextCategory();
+				System.out.println("Enter menu item's ID: ");
+				String foodID = sc.nextString();
+				System.out.println("Enter menu item's price: ");
+				float foodPrice = sc.nextFloat();
+				System.out.println("Enter menu item's description: ");
+				String foodDescription = sc.nextString();
+				
+				alaCartes.add(new alaCarte(foodName, foodCategory, foodID, foodPrice, foodDescription);
+				break;
+			case 2:
+			      //to be continued
+					      }
 	}
+					     
 
 	public void removeMenuItems() {
-		// TODO - implement Menu.removeMenuItems
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Choose which of the following to remove: ");
+		System.out.println("1) Ala Carte");
+		System.out.println("2) Package");
+		System.out.println("3) Promotion Package");
+		int choice = sc.nextInt();
+		
+		switch (choice){
+			case 1:
+				
+				
+				
 		throw new UnsupportedOperationException();
 	}
 
