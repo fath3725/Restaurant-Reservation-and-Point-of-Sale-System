42b1cf04c6ed5a081ed9d45a6994a5dc0b695bd7
Ryan Taw <ryan.taw96@gmail.com>
2021-11-01 20:17:42 +0800

    Added JavaDocs for menuitem alacarte and promotionpackage

--- AlaCarte.java
+++ AlaCarte.java
@@ -2,16 +2,16 @@ public class AlaCarte extends MenuItem {
 
 	private AlaCarteCategory category;
 	/**
-	 * Creates a new AlaCarte with the given id, name, price and description, quantity and category.
-	 * @param id This AlaCarte's id.
+	 * Creates a new AlaCarte with the given alacarteId, name, price and description, quantity and category.
+	 * @param alacarteId This AlaCarte's alacarteId.
 	 * @param name This AlaCarte's name.
 	 * @param price This AlaCarte's price.
 	 * @param description This AlaCarte's description.
 	 * @param quantity This AlaCarte's quantity.
 	 * @param category This AlaCarte's category.
 	 */
-	public AlaCarte(int id,String name, float price, String description,int quantity,AlaCarteCategory category) {
-		super(id,name,price,description,quantity);
+	public AlaCarte(int alacarteId,String name, float price, String description,int quantity,AlaCarteCategory category) {
+		super(alacarteId,name,price,description,quantity);
 		this.category=category;
 	}
 	/**
--- PromotionPackage.java
+++ PromotionPackage.java
@@ -4,16 +4,16 @@ public class PromotionPackage extends MenuItem {
 	//alacarte and promotionpackage have an association rlnshp
 	private ArrayList<AlaCarte> packageItems;
 	/**
-	 * Creates a new PromotionPackage with the given id, name, price and description and quantity.
-	 * @param id This PromotionPackage's id.
+	 * Creates a new PromotionPackage with the given packageId, name, price and description and quantity.
+	 * @param packageId This PromotionPackage's packageId.
 	 * @param name This PromotionPackage's name.
 	 * @param price This PromotionPackage's price.
 	 * @param description This PromotionPackage's description.
 	 * @param quantity This PromotionPackage's quantity.
 	 * @param ArrayList<AlaCarte>packageItems This PromotionPackage's arraylist of AlaCartes
 	 */
-	public PromotionPackage(int id,String name,float price,String description,int quantity,ArrayList<AlaCarte> packageItems) {
-		super(id,name,price,description,quantity);
+	public PromotionPackage(int packageId,String name,float price,String description,int quantity,ArrayList<AlaCarte> packageItems) {
+		super(packageId,name,price,description,quantity);
 		this.packageItems=packageItems;
 	}
 
